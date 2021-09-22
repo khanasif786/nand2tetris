a69eea6402807e127a97c100c634ff02b5358092
Asif Khan <asif.kgauri@gmail.com>
2021-09-22 21:20:31 +0530

    Completed mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,51 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+//pseudo Code
+//i=0
+//sum=0
+//R2=0
+//for(i=1;i<=R1;i++)
+//{
+//    sum=sum+R0
+//}
+//R2=sum
+
+//initializing i with 1
+@1
+D=A
+@i
+M=D
+
+//initializing R2 with 0
+@R2
+M=0
+
+(LOOP)
+    // if i > R1 than go to END
+    @i
+    D=M
+    @R1
+    D=M-D
+    @END
+    D;JLT
+    
+    //increment in i
+    @i
+    M=M+1
+
+    @R2
+    D=M
+    @R0
+    D=D+M
+    @R2
+    M=D
+
+    @LOOP
+    0,JMP
+
+(END)
+    @END
+    0;JMP
+
